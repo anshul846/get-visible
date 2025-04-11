36e714275b2b5a61e525c453b1d8f9f2125a430d
unknown <singalanshul846@gmail.com>
2025-04-11 20:46:10 +0530

    add root

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div className=" font-Montserrat ">Navbar</div>;
+  return <div className=" font-Montserrat  ">Navbar</div>;
 };
 
 export default Navbar;
