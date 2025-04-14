58569e70de6d278df368d444591fe3a61f4f6f28
rohit <rohitsiwachs1999@.com>
2025-04-14 09:06:15 +0530

    navbar and footer added

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import Footer from "@/components/common/Footer";
 import Navbar from "@/components/common/navbar";
 
 export default function Home() {
   return (
     <>
       <Navbar />
+      <Footer />
     </>
   );
 }
---
+++ src/components/common/Helper.jsx
@@ -0,0 +1,53 @@
+import {
+  FooterCallIcon,
+  FooterFacebookIcon,
+  FooterInstagramIcon,
+  FooterMailIcon,
+  FooterPinterstIcon,
+} from "../home-page/Icon";
+
+export const NavLinks = [
+  { label: "Agency", href: "#" },
+  { label: "Featured Work", href: "#" },
+  { label: "Web Design", href: "#" },
+  { label: "RE-Branding", href: "#" },
+  { label: "Products", href: "#" },
+  { label: "Contact Us", href: "#" },
+];
+export const footerNavLinks = [
+  { label: "Agency", href: "#" },
+  { label: "Featured Work", href: "#" },
+  { label: "Web Design", href: "#" },
+  { label: "Re-Branding", href: "#" },
+  { label: "Products", href: "#" },
+  { label: "Contact Us", href: "#" },
+];
+
+export const contactInfo = [
+  {
+    icon: <FooterMailIcon />,
+    label: "info@justgetvisible.com",
+    href: "mailto:info@justgetvisible.com",
+    underline: true,
+  },
+  {
+    icon: <FooterCallIcon />,
+    label: "0208 068 4939",
+    href: "tel:02080684939",
+  },
+];
+
+export const socialLinks = [
+  {
+    icon: <FooterPinterstIcon />,
+    href: "https://www.pinterest.com/justgetvisible",
+  },
+  {
+    icon: <FooterFacebookIcon />,
+    href: "https://www.facebook.com/justgetvisible",
+  },
+  {
+    icon: <FooterInstagramIcon />,
+    href: "https://www.instagram.com/justgetvisible",
+  },
+];
