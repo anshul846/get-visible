43b7af954ea55116eb3162c498369e745fa94adb
Rohit-singhal-crypto <rohityadavrohit8361@gmail.com>
2025-04-17 16:05:51 +0530

    section

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
 import Footer from "@/components/common/Footer";
-import Navbar from "@/components/common/navbar";
+import Navbar from "@/components/common/Navbar";
 
 export default function Home() {
   return (
     <>
-      {/* <Navbar />
-      <Footer /> */}
+      <Navbar />
+      <Footer />
     </>
   );
 }
